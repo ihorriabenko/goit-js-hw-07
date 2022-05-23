1a1e6fe569132a4b0ff219f6ffbf3fd911eb2b6a
IgorRyabenko <riabenko.igor@gmail.com>
2022-05-23 15:25:15 +0300

    1part done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const imgMarkup = createImgMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', imgMarkup);
+
+function createImgMarkup(items) {
+    return items.map(
+        ({
+            original,
+            preview,
+            description,
+        }) => `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>`,
+    ).join('');
+}
