ee743ab924fb28d600b7816a67fe2f87740ee33d
IgorRyabenko <riabenko.igor@gmail.com>
2022-05-25 17:34:08 +0300

    hw7 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// refs
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
 
+// create markup
+const imgMarkup = createImgMarkup(galleryItems);
+
+refs.gallery.innerHTML = imgMarkup;
+
+function createImgMarkup(items) {
+  return items
+    .map(
+      ({
+        original,
+        preview,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description} " />
+    </a>`,
+    )
+    .join('');
+}
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
